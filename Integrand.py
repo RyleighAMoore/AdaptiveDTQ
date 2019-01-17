df73a54d483e4366d1b4393831be8c51b0a3943c
Ryleigh <rmoore@math.utah.edu>
2019-01-17 13:50:13 -0700

    Added option to plot epsilon graph.

--- Integrand.py
+++ Integrand.py
@@ -1,7 +1,8 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 # This function is used to determine if more rows are needed in the kernel matrix G.
-# Basically, it checks that we are maintaining zero at the boundaries.
+# Basically, it checks that we are maintaining zero at the boundaries of the integrand.
 def computeEpsilon(G, phat):
     tol = 0
     val1 = np.max(G[:,0]*phat[0])
@@ -11,3 +12,10 @@ def computeEpsilon(G, phat):
     else:
         val = np.log(np.max(G[:, 0] * phat[0] + G[:, -1] * phat[-1]))
         return val
+
+
+def calculateIntegrand(G, phat):
+    val = np.zeros([phat.size, phat.size])
+    for i in range(np.size(phat)):
+        val[:,i]= G[:, i] * phat[i]
+    return val
