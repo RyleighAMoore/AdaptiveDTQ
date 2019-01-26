Edited epsilon calculation to fix small problem.

Integrand.py
@@ -18,13 +18,11 @@ def computeEpsilon3(G, phat):
     val = np.zeros(len(G))
     for w in range(len(G)-1):
         val[w] = np.max(G[w,:]*phat[w])
-        if val[w] <= 0:
-           val[w] = -np.inf
     return np.log(np.sum(np.abs(val)))
 
 
 def calculateIntegrand(G, phat):
     val = np.zeros([np.size(G, 0), np.size(G, 1)])
     for i in range(np.size(G, 1)):
-        val[i,:]= G[i,:] * phat[i]
+        val[:,i]= G[i,:] * phat[i]
     return val

simpleDTQ.py
@@ -51,9 +51,9 @@ assert numsteps > 0, 'The variable numsteps must be greater than 0'
 
 # define spatial grid
 k = h ** s
-# k = 0.1
-xMin = -1
-xMax = 1
+#k = 0.01
+xMin = -20
+xMax = 20
 xvec = np.arange(xMin, xMax, k)
 
 # Kernel matrix
@@ -126,7 +126,7 @@ if animateIntegrand:
         integrand = Integrand.calculateIntegrand(G_history[step], pdf_trajectory[step])
         Y = np.zeros([np.size(xvec_trajectory[step]), np.size(integrand,1)])
         for i in range(np.size(integrand,1)):
-            Y[:, i] = xvec_trajectory[step]
+            Y[i, :] = xvec_trajectory[step]
         l.set_xdata(Y)
         l.set_ydata(integrand)
         return l,
@@ -178,4 +178,5 @@ plt.figure()
 for j,i in enumerate(G_history):
     w = i.shape[0]
     plt.plot(j, w,'.')
+plt.title("Size of G Evolution")
 plt.show()