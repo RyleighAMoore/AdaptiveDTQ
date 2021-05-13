608d2397c4a21d9aafeab748a2933e8938f8b152
rmoore <rmoore@math.utah.edu>
2021-05-13 15:53:19 -0600

    removed unneeded code

--- EXAMPLE_RelativeTiming.py
+++ EXAMPLE_RelativeTiming.py
@@ -103,16 +103,16 @@ for i in x:
     count = count+1
     
 import pickle
-with open('L2wErrorArrayT115n.pickle', 'wb') as f:
+with open('L2wErrorArrayT115nn.pickle', 'wb') as f:
     pickle.dump(L2wErrorArrayT, f)
 f.close()
-with open('L2wErrorArray115n.pickle', 'wb') as f:
+with open('L2wErrorArray115nn.pickle', 'wb') as f:
     pickle.dump(L2wErrorArray, f)
 f.close()
-with open('TimingArrayT115n.pickle', 'wb') as f:
+with open('TimingArrayT115nn.pickle', 'wb') as f:
     pickle.dump(np.asarray(TimingArrayT), f)
 f.close()
-with open('TimingArray115n.pickle', 'wb') as f:
+with open('TimingArray115nn.pickle', 'wb') as f:
     pickle.dump(np.asarray(TimingArray), f)
 f.close()
     
